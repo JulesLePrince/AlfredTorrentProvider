15abe9541a2d67e041fea543722f9302e1df5750
Jules Le Prince <juleslp35740@gmail.com>
2024-07-13 21:58:15 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,39 @@
+from typing import Union
+from fastapi import FastAPI
+from typing import Optional
+from fastapi.responses import FileResponse
+from TorrentProviders import yts, ygg
+from Utils import tmdb_utils
+from unidecode import unidecode
+
+
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+@app.get("/filetest")
+def read_root():
+    return FileResponse(path="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4", filename="bunny", media_type='text/mp4')
+
+@app.get("/movie/get_torrent/{tmdb_id}")
+def read_item(tmdb_id: int, quality:str="1080p"):
+    match quality:
+        case "720p":
+            quality_wanted = 0
+        case "1080p" :
+            quality_wanted = 1
+        case "4k":
+            quality_wanted = 2
+        case _:
+            quality_wanted = 1
+
+    movie = tmdb_utils.Movie(movie_id=tmdb_id)
+    #torrent_movie_provider = yts.YtsTorrentProvider(base_yts_url="https://yts.mx", movie_infos=movie.data, quality_wanted=quality_wanted)
+    torrent_movie_provider = ygg.YggTorrentMovieProvider("https://www.ygg.re", "Radarr_Alfred", "Jules2005", movie.data, quality=quality_wanted)
+
+    torrent_title = unidecode(movie.data['en']['title']).replace(' ', '_').replace("'", "_").lower()
+    file_path = torrent_movie_provider.download(path="/Users/julesleprince/Developer/AlfredTorrentProvider/cachedTorrents", torrent_name=torrent_title)
+
+    return FileResponse(path=file_path, filename=f"{torrent_title}.torrent", media_type='text/torrent')
